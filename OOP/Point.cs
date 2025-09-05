83c8a9ccbfc443b56b72ae81c7c6ada3b3a31175
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2025-09-06 00:12:24 +0500

    Point.Distance DONE

---
+++ OOP/Point.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        //double x;
+        //double y;
+
+        ////Properties^
+        //public double X
+        //{
+        //    get { return x; }
+        //    set { x = value; }
+        //}
+        //public double Y
+        //{
+        //    get { return y; }
+        //    set { y = value; }
+        //}
+
+        ////Get/Set:
+        //public double GetX()
+        //{
+        //    return x;
+        //}
+        //public double GetY()
+        //{
+        //    return y;
+        //}
+        //public void SetX(double x)
+        //{
+        //    this.x = x;
+        //}
+        //public void SetY(double y)
+        //{
+        //    this.y = y;
+        //}
+
+        //      Methods
+        public void Print()
+        {
+            Console.WriteLine($"X = {X}, Y = {Y}");
+            //Console.WriteLine($"X = {GetX()}, Y = {GetY()}");
+        }
+
+        public double Distance(Point other)
+        {
+            return Math.Sqrt(Math.Pow(this.X - other.X, 2)+ Math.Pow(this.Y - other.Y, 2));
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,7 +10,27 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            //Point point = new Point();
+            //point.SetX(2);
+            //point.SetY(3);
+            //point.Print();
 
+            //point.X = 7;
+            //point.Y = 8;
+            //Console.WriteLine($"Properties: X = {point.X}, Y = {point.Y}");
+
+            Point A = new Point();
+            Console.WriteLine("Введите координаты точки A, расстояние до которой требуется определить:");
+            Console.Write("X = ");
+            A.X = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y = ");
+            A.Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"X = {A.X}, Y = {A.Y}");
+            Point B = new Point();
+            B.X = 5;
+            B.Y = 6;
+            Console.WriteLine($"Координаты исходной точки B: X = {B.X}, Y = {B.Y}");
+            Console.WriteLine($"Расстояние от исходной точки B до точки А: {B.Distance(A)}");
         }
     }
 }
