caf8d0d8909ad6374c81f2fc8ab5736bcf216c67
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2025-09-06 14:11:41 +0500

    Fraction_class DONE

---
+++ Fraction_class/Fraction.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction_class
+{
+    internal class Fraction
+    {
+        public int Integer { get; set; }
+        public int Numerator { get; set; }
+        int denominator;            //smallCamel        //переменная-член класса (поле класса)
+        public int Denominator      //BigCamel          //свойство (property)
+        {
+            get => denominator;
+            /*get
+            {
+                return denominator;
+            }*/
+            set => denominator = value == 0 ? 1 : value;
+            /*set
+            {
+                denominator = value == 0 ? 1 : value;
+                //if (value != 0) denominator = value;
+                //else denominator = 1;
+            }*/
+        }
+
+        //Constructors:
+        public Fraction()
+        {
+            Console.WriteLine($"DefaultConstructor:\t\t {this.GetHashCode().ToString("X")}");
+        }
+        public Fraction (int Integer)
+        {
+            this.Integer = Integer;
+            this.Denominator = 1;
+            Console.WriteLine($"SingleArgumentConstructor:\t {GetHashCode().ToString("X")}");
+        }
+        public Fraction(int Numerator, int Denominator)
+        {
+            this.Numerator = Numerator;
+            this.Denominator = Denominator;
+            Console.WriteLine($"Costructor:\t\t\t {GetHashCode().ToString("X")}");
+        }
+        public Fraction(int Integer, int Numerator, int Denominator)
+        {
+            this.Integer=Integer;
+            this.Numerator = Numerator;
+            this.Denominator = Denominator;
+            Console.WriteLine($"Costructor:\t\t\t {GetHashCode().ToString("X")}");
+        }
+        public Fraction (Fraction other)
+        {
+            this.Integer = other.Integer;
+            this.Numerator=other.Numerator;
+            this.Denominator=other.Denominator;
+            Console.WriteLine($"CopyCostructor:\t\t\t {GetHashCode().ToString("X")}");
+        }
+        ~Fraction()
+        {
+            Console.WriteLine($"Destructor:\t\t\t {this.GetHashCode().ToString("X")}");
+        }
+
+        //Operators:
+        public static Fraction operator *(Fraction left, Fraction right) => 
+            new Fraction
+                (
+                left.ToImproper().Numerator * right.ToImproper().Numerator,
+                left.denominator * right.denominator
+                ).ToProper();
+        /*{
+            Fraction l_buffer = new Fraction(left);
+            Fraction r_buffer = new Fraction(right);
+            l_buffer.ToImproper();
+            r_buffer.ToImproper();
+            return new Fraction
+                (
+                l_buffer.Numerator* r_buffer.Numerator,
+                l_buffer.Denominator* r_buffer.Denominator
+                ).ToProper();
+        }*/
+        public static Fraction operator /(Fraction left, Fraction right) => left * right.Inverted();
+
+        //Methods
+        public Fraction ToImproper() => new Fraction(Numerator + Integer * Denominator, Denominator);
+        /*{
+            return new Fraction(Numerator + Integer * Denominator, Denominator);
+            //Fraction improper = new Fraction(this);
+            //improper.Numerator += Integer * Denominator;
+            //improper.Integer = 0;
+            //return improper;
+        }*/
+        public Fraction ToProper() => new Fraction(Numerator / Denominator, Numerator %= Denominator, Denominator);
+        /*{
+            return new Fraction(Numerator / Denominator, Numerator %= Denominator, Denominator);
+            //proper.Integer += Numerator / Denominator;
+            //proper.Numerator %= Denominator;
+            //if (Numerator < 0) proper.Numerator *= -1;
+            //return proper;
+        }*/
+        public Fraction Inverted() => new Fraction(Denominator, ToImproper().Numerator);
+        /*{
+            Fraction inverted = ToImproper();
+            return new Fraction
+                (
+                inverted.Denominator, inverted.Numerator
+                );
+        }*/
+        public void Print() => Console.WriteLine($"{Integer}({Numerator}/{Denominator})");
+        /*{
+            Console.WriteLine($"{Integer}({Numerator}/{Denominator})");
+        }*/
+
+                //Overrides:
+
+        public override string ToString()
+        {
+            string @string = "";
+            if (Integer != 0) @string += $"{Integer}";
+            if (Numerator != 0)
+            {
+                if (Integer != 0) @string += "(";
+                @string += $"{Numerator}/{Denominator}";
+                if (Integer != 0) @string += ")";
+            }
+            else if (Integer == 0) @string = "0";
+            return @string;
+        }
+    }
+}
---
+++ Fraction_class/Program.cs
@@ -0,0 +1,58 @@
+﻿//#define CONSTRUCTORS_CHECK
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction_class
+{
+    internal class Program
+    {
+        const string delimiter = "\n------------------------------------\n";
+        static void Main(string[] args)
+        {
+#if CONSTRUCTORS_CHECK
+            Fraction A = new Fraction();
+            Console.WriteLine(A);
+            A.Print();
+            //Console.WriteLine(A.GetType());
+
+            Fraction B = new Fraction(5);
+            Console.WriteLine(B);
+            B.Print();
+
+            Fraction C = new Fraction(1, 2);
+            Console.WriteLine(C);
+            C.Print();
+
+            Fraction D = new Fraction(2, 3, 4);
+            Console.WriteLine(D);
+            D.Print();
+
+            Console.WriteLine("\n--------------------------------------\n");
+
+            Fraction E = new Fraction(D);
+            Console.WriteLine(E);
+            E.Print();
+#endif
+            Fraction A = new Fraction(2, 3, 4);
+            Fraction B = new Fraction(3, 4, 5);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(delimiter);
+            Fraction C = A * B;
+            Console.WriteLine(delimiter);
+            Console.WriteLine(C);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(delimiter);
+            C = A / B;
+            Console.WriteLine(delimiter);
+            Console.WriteLine(C);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+        }
+    }
+}
